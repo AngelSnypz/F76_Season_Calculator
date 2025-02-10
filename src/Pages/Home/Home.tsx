138fd3028c2a1ffeaaea146980566f1e117ea8d6
AngelSnypz <fal13nang3l@hotmail.com>
2025-02-10 22:26:24 +1300

    homepage added with basic link redirects

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const Home = () => {
-  return <div>TODO Home</div>;
+  return (
+    <>
+      Welcome to AngelSnypz Fallout76 Tools
+      <div>
+        <Link to="/scoreCalc">Go to Score Calculator</Link>
+      </div>
+      <div>
+        <Link to="/builder">Go to Builder</Link>
+      </div>
+    </>
+  );
 };
